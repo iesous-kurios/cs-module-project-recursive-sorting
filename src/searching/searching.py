6afe3bb0cce65dbfb61db2e98c0942590520adfe
iesous-kurios <j.wylie.81@gmail.com>
2020-10-05 16:17:32 -0700

    passing search test file

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,20 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    l = start
+    h = end
+    if l <= h:
 
+        m = (l + h) // 2
+        print('l', l, 'h', h, 'm', m)
+        if arr[m] == target:
+            return m
+        elif arr[m] < target:
+            return binary_search(arr, target, m + 1, h)
+        else:
+            return binary_search(arr, target, l, m - 1)
+    else:
+        return -1
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -10,5 +23,21 @@ def binary_search(arr, target, start, end):
 # You can implement this function either recursively 
 # or iteratively
 def agnostic_binary_search(arr, target):
-    # Your code here
+    l, h = 0, len(arr) - 1
+    def rev_binary_search(arr, target, l, h):
+        if l >= h:
+            m = (l + h) // 2
+            print('l', l, 'h', h, 'm', m)
+            if arr[m] == target:
+                return m
+            elif arr[m] < target:
+                return rev_binary_search(arr, target, m - 1, h)  
+            else:
+                return rev_binary_search(arr, target, l, m + 1)                                 
+        else:
+            return -1
+    if arr[l] > arr[h]:
+        return rev_binary_search(arr, target, h, l)
+    else:
+        return binary_search(arr, target, l, h)
 
